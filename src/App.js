06cd97e0adeed3b2b9b95526078bcb0cd7b469d3
GYegor <yahor2011@gmail.com>
2019-08-10 20:56:00 +0300

    feat: add state, components, methods to App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import Websocket from 'react-websocket';
+import MessageList from "./MessageList";
+import SendMessage from "./SendMessage";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ws: new WebSocket('ws://st-chat.shas.tel'),
+      result: [],
+      inputMessage: ''
+    };
+  }
+
+  getMessage = () => {
+    const websocket = this.state.ws;
+    websocket.onmessage = (e) => {
+    const newData = JSON.parse(e.data)
+      newData.reverse();
+      this.setState( (prevState) => {return {result: [...prevState.result,...newData]};});
+    }
+  }
+
+  sendMessage = (message) => {
+    let websocket = this.state.ws;
+    const messageObject = {
+      from: 'GY',
+      message: message
+    }
+    websocket.send(JSON.stringify(messageObject));
+    console.log(JSON.stringify(messageObject));
+  }
+
+
+  handleSubmit = (message) => {
+    this.sendMessage(message);
+  }
+
+
+  componentDidMount() {
+    this.getMessage();
+  }
+
+
+  componentDidUpdate() {
+    // this.onMessage();
+    window.scrollTo(0, document.body.scrollHeight);
+  }
+
+  handleData = (data) => {
+    let newData = JSON.parse(data);
+    console.log(newData);
+    // console.log(newDta);
+  }
+
+
+  render() {
+    return (
+    <div className="main">
+      <MessageList data={this.state.result}  />
+      {/* <Websocket url='ws://st-chat.shas.tel' onMessage={this.handleData} /> */}
+      <SendMessage handleSubmit={this.handleSubmit} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
