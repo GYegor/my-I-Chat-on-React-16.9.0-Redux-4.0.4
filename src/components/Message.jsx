4e68523e144655b18519301fc8d0f80f49c379db
GYegor <yahor2011@gmail.com>
2019-08-17 08:34:30 +0300

    refactor: Message component, extend notification time-out

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { PureComponent } from 'react';
 import customDateString from '../utils/customDateString';
 
 
-function Message({ message }) {
-  return (
-    <li>
-      <span className="name">{`${message.from}:   `}</span>
-      <span className="time">{customDateString(message.time)}</span>
-      <p className="message-body">{message.message}</p>
-    </li>
-  );
+class Message extends PureComponent {
+  render() {
+    const { message } = this.props;
+    return (
+      <li>
+        <span className="name">{`${message.from}:   `}</span>
+        <span className="time">{customDateString(message.time)}</span>
+        <p className="message-body">{message.message}</p>
+      </li>
+    );
+  }
 }
-
 export default Message;
